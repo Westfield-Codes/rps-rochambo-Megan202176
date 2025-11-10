74e247d173f84913ef44e50461496221bc5ecea6
Megan202176 <megan202176@student-schoolsofwestfield.org>
2025-11-10 17:10:23 +0000

    changes

--- script.js
+++ script.js
@@ -8,3 +8,62 @@
 *     d. Move to next function
 *  3. System Test finished version (does it work right in all conditions?)
 */
+/* Global Variables */
+let score = [0,0];
+
+function main(){
+let u=""
+let c=""
+while(u=c){
+    u=userTurn()
+    c=cpuTurn()
+    if (u==c)alert("We both chose"+ c)
+}
+let combo=u+c
+ winner=findWinner(combo)
+ ("You chose "+ u+" and I chose "+ c+ winner+" won!")
+}
+
+function setRounds() {
+
+}
+
+/* RPS Round
+* plays a round of RPS and tells the winner
+* @param: none
+* @return:none
+*/
+function rpsRound() {
+
+}
+
+/* userturn
+* user can choose r, p, or s.
+* if bad Input, give new choice
+* @param:none
+* @return:choice
+*/
+function userTurn() {
+return("r")
+}
+
+/* cpuTurn
+* computer choose between r, p, or s
+* @param:none
+* @return: choice
+*/
+function cpuTurn() {
+return ("p")
+}
+
+/* findWinner
+* takes user and computer turn
+* decides who the winner is
+* returns winner
+* @param:u,c
+* @return: winner
+*/
+function findWinner(u,c) {
+let u="r"
+let c="p"
+}
